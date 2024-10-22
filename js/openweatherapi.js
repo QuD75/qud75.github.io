a5e9217c4bbe93bdc9f48d0514e9aaaaa06b0fdd
Quentin Dusserre <quentin.dusserre@ekwateur.fr>
2024-10-22 17:25:50 +0200

    init direction vent

--- js/openweatherapi.js
+++ js/openweatherapi.js
@@ -151,7 +151,7 @@ document.addEventListener('DOMContentLoaded', () => {
             const windDirectionIcon = document.createElement('img');
             windDirectionIcon.style.width = "30px";
             windDirectionIcon.style.height = "30px";
-            windDirectionIcon.src = "icons/question-mark.png";
+            windDirectionIcon.src = getWindDirectionIcon(data.windDirection);
             windDirectionCell.appendChild(windDirectionIcon);
             windDirectionRow.appendChild(windDirectionCell);
 
@@ -164,31 +164,65 @@ document.addEventListener('DOMContentLoaded', () => {
             const weatherIcon = document.createElement('img');
             weatherIcon.style.width = "30px";
             weatherIcon.style.height = "30px";
-            if (item.weather === "01d") weatherIcon.src = "icons/01d@2x.png";
-            else if (item.weather === "01n") weatherIcon.src = "icons/01n@2x.png";
-            else if (item.weather === "02d") weatherIcon.src = "icons/02d@2x.png";
-            else if (item.weather === "02n") weatherIcon.src = "icons/02n@2x.png";
-            else if (item.weather === "03d") weatherIcon.src = "icons/03d@2x.png";
-            else if (item.weather === "03n") weatherIcon.src = "icons/03n@2x.png";
-            else if (item.weather === "04d") weatherIcon.src = "icons/04d@2x.png";
-            else if (item.weather === "04n") weatherIcon.src = "icons/04n@2x.png";
-            else if (item.weather === "09d") weatherIcon.src = "icons/09d@2x.png";
-            else if (item.weather === "09n") weatherIcon.src = "icons/09n@2x.png";
-            else if (item.weather === "10d") weatherIcon.src = "icons/10d@2x.png";
-            else if (item.weather === "10n") weatherIcon.src = "icons/10n@2x.png";
-            else if (item.weather === "11d") weatherIcon.src = "icons/11d@2x.png";
-            else if (item.weather === "11n") weatherIcon.src = "icons/11n@2x.png";
-            else if (item.weather === "13d") weatherIcon.src = "icons/13d@2x.png";
-            else if (item.weather === "13n") weatherIcon.src = "icons/13n@2x.png";
-            else if (item.weather === "50d") weatherIcon.src = "icons/50d@2x.png";
-            else if (item.weather === "50n") weatherIcon.src = "icons/50n@2x.png";
-            else weatherIcon.src = "icons/question-mark.png";
+            weatherIcon.src(getWeatherIcon(data.weather));
             
             weatherCell.appendChild(weatherIcon);
             weatherRow.appendChild(weatherCell);
         });
 
         mergeDaysRow();
+    }
+
+    function getWeatherIcon(weather){
+        if (weather === "01d") return "icons/01d@2x.png";
+        else if (weather === "01n") return "icons/weather/01n@2x.png";
+        else if (weather === "02d") return "icons/weather/02d@2x.png";
+        else if (weather === "02n") return "icons/weather/02n@2x.png";
+        else if (weather === "03d") return "icons/weather/03d@2x.png";
+        else if (weather === "03n") return "icons/weather/03n@2x.png";
+        else if (weather === "04d") return "icons/weather/04d@2x.png";
+        else if (weather === "04n") return "icons/weather/04n@2x.png";
+        else if (weather === "09d") return "icons/weather/09d@2x.png";
+        else if (weather === "09n") return "icons/weather/09n@2x.png";
+        else if (weather === "10d") return "icons/weather/10d@2x.png";
+        else if (weather === "10n") return "icons/weather/10n@2x.png";
+        else if (weather === "11d") return "icons/weather/11d@2x.png";
+        else if (weather === "11n") return "icons/weather/11n@2x.png";
+        else if (weather === "13d") return "icons/weather/13d@2x.png";
+        else if (weather === "13n") return "icons/weather/13n@2x.png";
+        else if (weather === "50d") return "icons/weather/50d@2x.png";
+        else if (weather === "50n") return "icons/weather/50n@2x.png";
+        else return "icons/question-mark.png";
+    }
+
+    function getWindDirectionIcon(wind_deg) {
+        const directions = [
+            { min: 348.75, max: 360, icon: 'icons/wind/n.png' },
+            { min: 0, max: 11.25, icon: 'icons/wind/n' },
+            { min: 11.25, max: 33.75, icon: 'icons/wind/nne.png' },
+            { min: 33.75, max: 56.25, icon: 'icons/wind/ne.png' },
+            { min: 56.25, max: 78.75, icon: 'icons/wind/ene.png' },
+            { min: 78.75, max: 101.25, icon: 'icons/wind/e.png' },
+            { min: 101.25, max: 123.75, icon: 'icons/wind/ese.png' },
+            { min: 123.75, max: 146.25, icon: 'icons/wind/se.png' },
+            { min: 146.25, max: 168.75, icon: 'icons/wind/sse.png' },
+            { min: 168.75, max: 191.25, icon: 'icons/wind/s.png' },
+            { min: 191.25, max: 213.75, icon: 'icons/wind/sso.png' },
+            { min: 213.75, max: 236.25, icon: 'icons/wind/so.png' },
+            { min: 236.25, max: 258.75, icon: 'icons/wind/oso.png' },
+            { min: 258.75, max: 281.25, icon: 'icons/wind/o.png' },
+            { min: 281.25, max: 303.75, icon: 'icons/wind/ono.png' },
+            { min: 303.75, max: 326.25, icon: 'icons/wind/no.png' },
+            { min: 326.25, max: 348.75, icon: 'icons/wind/nno.png' }
+        ];
+    
+        for (const direction of directions) {
+            if (wind_deg >= direction.min && wind_deg < direction.max) {
+                return direction.icon;
+            }
+        }
+    
+        return 'icons/question-mark.png';
     }
 
     function getTemperatureColor(temperature) {
