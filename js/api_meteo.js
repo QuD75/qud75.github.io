b0126aedf7a8b68a876ff799dfae381779a36d5c
Quentin Dusserre <quentin.dusserre@ekwateur.fr>
2024-10-24 21:21:57 +0200

    fix direction vent

--- js/api_meteo.js
+++ js/api_meteo.js
@@ -157,13 +157,13 @@ document.addEventListener('DOMContentLoaded', () => {
             { min: 123.75, max: 146.25, icon: 'icons/wind/se.png' },
             { min: 146.25, max: 168.75, icon: 'icons/wind/sse.png' },
             { min: 168.75, max: 191.25, icon: 'icons/wind/s.png' },
-            { min: 191.25, max: 213.75, icon: 'icons/wind/ssw.png' },
-            { min: 213.75, max: 236.25, icon: 'icons/wind/sw.png' },
-            { min: 236.25, max: 258.75, icon: 'icons/wind/wsw.png' },
-            { min: 258.75, max: 281.25, icon: 'icons/wind/w.png' },
-            { min: 281.25, max: 303.75, icon: 'icons/wind/wnw.png' },
-            { min: 303.75, max: 326.25, icon: 'icons/wind/nw.png' },
-            { min: 326.25, max: 348.75, icon: 'icons/wind/nnw.png' }
+            { min: 191.25, max: 213.75, icon: 'icons/wind/sso.png' },
+            { min: 213.75, max: 236.25, icon: 'icons/wind/so.png' },
+            { min: 236.25, max: 258.75, icon: 'icons/wind/oso.png' },
+            { min: 258.75, max: 281.25, icon: 'icons/wind/o.png' },
+            { min: 281.25, max: 303.75, icon: 'icons/wind/ono.png' },
+            { min: 303.75, max: 326.25, icon: 'icons/wind/no.png' },
+            { min: 326.25, max: 348.75, icon: 'icons/wind/nno.png' }
         ];
         return directions.find(d => wind_deg >= d.min && wind_deg <= d.max)?.icon || 'icons/wind/unknown.png';
     }
