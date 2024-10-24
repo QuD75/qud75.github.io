baf38ecccae34335a0598e07d354ee2b806360d4
Quentin Dusserre <quentin.dusserre@ekwateur.fr>
2024-10-24 14:54:17 +0200

    cors

--- js/api_meteo.js
+++ js/api_meteo.js
@@ -4,12 +4,13 @@ const username = 'quentin_dusserre_quentin'; // Remplace par ton nom d'utilisate
 const password = 'nIg974UeEM'; // Remplace par ton mot de passe
 const apiUrl = 'https://api.meteomatics.com/2024-10-24T00:00:00Z--2024-10-27T00:00:00Z:PT1H/t_2m:C/47.2917,-2.5201/json';
 const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+const encodedCredentials = btoa(`${username}:${password}`)
 
 async function getApiData() {
     fetch(proxyUrl + apiUrl, {
         method: 'GET',
         headers: {
-            'Authorization': `Basic ${btoa('${username}:${password}')}`,
+            'Authorization': encodedCredentials,
             'Content-Type': 'application/json',
             'X-Requested-With': 'XMLHttpRequest'
         },
