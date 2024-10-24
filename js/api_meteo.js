055fc8aa98d97bc72c04f30dc1e936004502aa30
Quentin Dusserre <quentin.dusserre@ekwateur.fr>
2024-10-24 13:02:36 +0200

    cors

--- js/api_meteo.js
+++ js/api_meteo.js
@@ -1,41 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-// Variables pour le token et son expiration
-let authToken = null;
-let tokenExpiration = null;
-
-// Fonction pour authentifier l'utilisateur et obtenir le token
-async function authenticate() {
-    const response = await fetch('https://cors-anywhere.herokuapp.com/https://login.meteomatics.com/api/v1/token', {
-        method: 'GET',
-        headers: {
-            'Authorization': `Basic cXVlbnRpbl9kdXNzZXJyZV9xdWVudGluOm5JZzk3NFVlRU0=`
-        },
-    });
-
-    if (!response.ok) {
-        throw new Error('Authentication failed');
-    }
-
-    const data = await response.json();
-    authToken = data.token;
-    tokenExpiration = Date.now() + 2 * 60 * 60 * 1000 - 60 * 1000; // Marge
-}
-
 // Fonction pour récupérer des données avec le token
 async function fetchData() {
-    // Vérifier si le token est valide
-    if (!authToken || Date.now() >= tokenExpiration) {
-        // Si le token est expiré ou inexistant, ré-authentifier
-        await authenticate();
-    }
 
-    const response = await fetch('https://api.meteomatics.com/2024-10-24T00:00:00Z--2024-10-27T00:00:00Z:PT1H/t_2m:C/47.2917,-2.5201/json', {
-        method: 'GET',
-        headers: {
-            'Authorization': `Bearer ${authToken}`,
-            'Access-Control-Allow-Origin': "*"
-        },
+    const response = await fetch('https://quentin_dusserre_quentin:nIg974UeEM@api.meteomatics.com/2024-10-24T00:00:00Z--2024-10-27T00:00:00Z:PT1H/t_2m:C/47.2917,-2.5201/json', {
+        method: 'GET'
     });
 
     if (!response.ok) {
