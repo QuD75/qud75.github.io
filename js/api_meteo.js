f7766f2f8a1c7a1bc4b92f0bf3037de202bda67f
Quentin Dusserre <quentin.dusserre@ekwateur.fr>
2024-10-24 13:08:30 +0200

    cors

--- js/api_meteo.js
+++ js/api_meteo.js
@@ -1,5 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+const username = 'quentin_dusserre_quentin'; // Remplace par ton nom d'utilisateur
+const password = 'nIg974UeEM'; // Remplace par ton mot de passe
+const apiUrl = 'https://api.meteomatics.com/2024-10-24T00:00:00Z--2024-10-27T00:00:00Z:PT1H/t_2m:C/47.2917,-2.5201/json';
+
 // Fonction pour obtenir des données de l'API
 async function getApiData() {
     const encodedCredentials = btoa(`${username}:${password}`);
