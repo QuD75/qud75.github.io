b148cd92ae6b6d2e30d3ebe6fbf87fad4207fe1e
Quentin Dusserre <quentin.dusserre@ekwateur.fr>
2024-10-24 15:09:06 +0200

    cors

--- js/api_meteo.js
+++ js/api_meteo.js
@@ -7,27 +7,32 @@ const lon = '-2.5201';
 const params = 't_2m:C';
 const beginDate = '2024-10-24T00:00:00Z';
 const endDate = '2024-10-27T00:00:00Z';
-const apiUrl = `https://api.meteomatics.com/${beginDate}--${endDate}:PT1H/${params}/${lat},${lon}/json`;
+const apiUrl = `https://api.meteomatics.com/${beginDate}--${endDate}:PT1H/${params}/${lat},${lon}/json`;s
 const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-const encodedCredentials = btoa(`${username}:${password}`);
-    
-fetch(proxyUrl + apiUrl, {
-    method: 'GET',
-    headers: {
-        'Authorization': encodedCredentials,
-        'Content-Type': 'application/json',
-        'X-Requested-With': 'XMLHttpRequest'
-    },
-})
-.then(response => {
-    if (!response.ok) throw new Error(`Erreur HTTP: ${response.status}`);
-    return response.json();
-})
-.then(data => {
-    console.log(data);
-    // Traite tes données ici
-})
-.catch(error => console.error("Erreur lors de la récupération des données :", error));
 
+async function getApiData() {
+    const encodedCredentials = btoa(`${username}:${password}`);
+    fetch(proxyUrl + apiUrl, {
+        method: 'GET',
+        headers: {
+            'Authorization': encodedCredentials,
+            'Content-Type': 'application/json',
+            'X-Requested-With': 'XMLHttpRequest'
+        },
+    })
+    .then(response => {
+        if (!response.ok) throw new Error(`Erreur HTTP: ${response.status}`);
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+        // Traite tes données ici
+    })
+    .catch(error => console.error("Erreur lors de la récupération des données :", error));
+}
+
+
+// Appel de la fonction
+getApiData();
 
 });
