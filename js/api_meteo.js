fb7a3a6dcb7030137d1c5f50edb1d9bb2d3289ee
Quentin Dusserre <quentin.dusserre@ekwateur.fr>
2024-10-25 10:56:48 +0200

    back to croisic

--- js/api_meteo.js
+++ js/api_meteo.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
     const username = 'quentin_dusserre_quentin';
     const password = 'nIg974UeEM';
-    //const lat = '47.2917';
-    //const lon = '-2.5201';
-    const lat = '16.7665';
-    const lon = '-3.0025';
+    const lat = '47.2917';
+    const lon = '-2.5201';
+    //const lat = '16.7665';
+    //const lon = '-3.0025';
     const params = 't_2m:C,precip_1h:mm,wind_speed_10m:ms,wind_gusts_10m_1h:ms,wind_dir_10m:d,msl_pressure:hPa,weather_symbol_1h:idx,uv:idx';
 
     const currentDate = new Date();
