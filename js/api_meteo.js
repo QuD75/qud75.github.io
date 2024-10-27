16020d59a41a615852bf204fb11234cca44630ca
Quentin Dusserre <quentin.dusserre@ekwateur.fr>
2024-10-27 18:04:15 +0100

    graphique rafales vent

--- js/api_meteo.js
+++ js/api_meteo.js
@@ -434,17 +434,18 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 
-    function getWindChart(data) {
+    function getWindChart(wind, windGust) {
         const ctx = document.getElementById('wind-chart').getContext('2d');
 
-        const labels = data.dates.map(dateData => {
+        const labels = windGust.dates.map(dateData => {
             return new Date(dateData.date).toLocaleTimeString('fr-FR', { hour: '2-digit', minute: '2-digit' });
         });
 
-        const winds = data.dates.map(dateData => dateData.value);
-
+        const winds = wind.dates.map(dateData => dateData.value);
         const minWind = Math.min(...winds);
-        const maxWind = Math.floor(Math.max(...winds) + 1);
+
+        const windGusts = windGust.dates.map(dateData => dateData.value);
+        const maxWindGust = Math.floor(Math.max(...windGusts) + 1);
 
         const windChart = new Chart(ctx, {
             type: 'line',
@@ -453,6 +454,16 @@ document.addEventListener('DOMContentLoaded', () => {
                 datasets: [{
                     label: 'Vent (km/h)',
                     data: winds,
+                    borderColor: 'rgba(204, 153, 0, 1)', // Couleur de la ligne en jaune foncé
+                    backgroundColor: 'rgba(204, 153, 0, 0.2)', // Couleur de fond jaune foncé avec une opacité de 20%
+                    borderWidth: 3,
+                    pointRadius: 0,
+                    tension: 0.5, // Tension pour lisser la courbe
+                    cubicInterpolationMode: 'monotone' // Mode d'interpolation cubique pour la courbe lissée
+                },
+                {
+                    label: 'Vent (km/h)',
+                    data: windGusts,
                     borderColor: 'rgba(204, 153, 0, 1)', // Couleur de la ligne en jaune foncé
                     backgroundColor: 'rgba(204, 153, 0, 0.2)', // Couleur de fond jaune foncé avec une opacité de 20%
                     borderWidth: 3,
@@ -487,7 +498,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     },
                     y: {
                         min: minWind, // Valeur minimum ajustée
-                        max: maxWind, // Valeur maximum ajustée
+                        max: maxWindGust, // Valeur maximum ajustée
                         title: {
                             display: true,
                             text: 'Vent (km/h)'
