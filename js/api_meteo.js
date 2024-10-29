c4586dd09aaf266d3ca5ea0d25cdd8c0bbf814bf
Quentin Dusserre <quentin.dusserre@ekwateur.fr>
2024-10-29 16:46:06 +0100

    message d'attentes en-dessous des titres

--- js/api_meteo.js
+++ js/api_meteo.js
@@ -88,7 +88,7 @@ document.addEventListener('DOMContentLoaded', () => {
         // Masquer le message de chargement et afficher les conteneurs des jours et de la semaine
         document.getElementById("loading-message-day").style.display = "none";
         document.getElementById("day-container-tab").style.display = "block";
-        document.getElementById("week-container-graphs").style.display = "block";
+        document.getElementById("day-container-graphs").style.display = "block";
 
         fillTableDay(dataDay);
 
