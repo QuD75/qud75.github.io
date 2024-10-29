693ec679762b0ea7dfeba11013cad96acbc8d6fe
Quentin Dusserre <quentin.dusserre@ekwateur.fr>
2024-10-29 23:00:28 +0100

    couleur temperature

--- js/api_meteo.js
+++ js/api_meteo.js
@@ -640,7 +640,7 @@ document.addEventListener('DOMContentLoaded', () => {
     
         // Calcul de la luminosité
         const luminosity = 0.299 * r + 0.587 * g + 0.114 * b;
-        return luminosity < 128 ? 'white' : 'black';
+        return luminosity < 120 ? 'white' : 'black';
     }
 
     getApiData(getMockValue());
