21f1d2a1d3ebcb4c96b2b579e4f7b325f24264b2
Quentin Dusserre <quentin.dusserre@ekwateur.fr>
2024-10-30 10:48:48 +0100

    ajustement couleur pluie

--- js/api_meteo.js
+++ js/api_meteo.js
@@ -271,7 +271,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
 
         fillWeatherRow(data.data[0], 0, 1, null, temperatureRow, getTempRainWindColor, -5, 40, 300, 0);
-        fillWeatherRow(data.data[1], 1, 1, null, rainRow, getTempRainWindColor, 0, 5, 180, 240, true);
+        fillWeatherRow(data.data[1], 1, 1, null, rainRow, getTempRainWindColor, 0, 5, 180, 240, "true");
         fillWeatherRow(data.data[2], 0, 3.6, 5, windRow, getTempRainWindColor, 0, 100, 210, 0);
         fillWeatherRow(data.data[3], 0, 3.6, 5, windGustRow, getTempRainWindColor, 0, 100, 210, 0);
         fillWindDirectionRow(data.data[4], windDirectionRow);
@@ -344,7 +344,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         fillWeatherRow(data.data[2], 0, 1, 1, tempMinRow, getTempRainWindColor, -5, 40, 300, 0);
         fillWeatherRow(data.data[3], 0, 1, 1, tempMaxRow, getTempRainWindColor, -5, 40, 300, 0);
-        fillWeatherRow(data.data[4], 1, 1, 1, rainRow, getTempRainWindColor, 0, 5, 180, 240, true);
+        fillWeatherRow(data.data[4], 1, 1, 1, rainRow, getTempRainWindColor, 0, 5, 180, 240, "true");
         fillWeatherRow(data.data[5], 0, 3.6, 5, windRow, getTempRainWindColor, 0, 100, 210, 0);
         fillSymbolRow(data.data[6], weatherRow);
     }
@@ -510,9 +510,10 @@ document.addEventListener('DOMContentLoaded', () => {
     //Fonctions de coloriage
     function getTempRainWindColor(value, minValue, maxValue, hueMin, hueMax, rain) {
         let color;
+        const isRain = (typeof rain === "string" ? rain.toLowerCase() === "true" : Boolean(rain));
         const numericValue = Number(value);
-        if (rain) console.log("Value : " + value);
-        if (numericValue < 0.001 && rain) {
+        if (isRain) console.log("Value : " + value);
+        if (numericValue < 0.001 && isRain) {
             console.log("Valeur nulle");
             color = `hsl(0, 0%, 100%)`
         } else if (numericValue < minValue) {
