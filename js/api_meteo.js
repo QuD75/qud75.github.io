934ab6ab2a099b3b2bd6470cbd176281eedb11f1
Quentin Dusserre <quentin.dusserre@ekwateur.fr>
2024-11-02 18:43:39 +0100

    découpage js

--- js/api_meteo.js
+++ js/api_meteo.js
@@ -517,26 +517,5 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 
-    //Fonction pour le format des dates
-    function formatDate(date, hasYear, hasMonth, hasDay, hasHour, hasMinute) {
-        const options = {
-            year: hasYear ? 'numeric' : undefined,
-            month: hasMonth ? '2-digit' : undefined,
-            day: hasDay ? '2-digit' : undefined,
-            hour: hasHour ? '2-digit' : undefined,
-            minute: hasMinute ? '2-digit' : undefined,
-            hour12: false,
-        };
-        let formattedDate = date.toLocaleString('fr-FR', options);
-        if (hasHour && !hasMinute) {
-            let length = formattedDate.length;
-            formattedDate = formattedDate.slice(0, -2) + formattedDate.charAt(length - 1);
-            if (formattedDate.charAt(length - 4) === '0') {
-                formattedDate = formattedDate.slice(0, -3) + formattedDate.slice(-2);
-            }
-        }
-        return formattedDate;
-    }
-
     getApiData(getMockValue());
 });
