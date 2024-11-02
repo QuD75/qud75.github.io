180bdad49ca9568870decbc8b21f96937094f290
Quentin Dusserre <quentin.dusserre@ekwateur.fr>
2024-11-02 12:20:07 +0100

    symbole meteo

--- js/api_meteo.js
+++ js/api_meteo.js
@@ -470,8 +470,9 @@ document.addEventListener('DOMContentLoaded', () => {
             weatherIcon.style.width = "auto";
             weatherIcon.style.height = "100%";
             weatherIcon.style.maxHeight = "100%";
+            weatherIcon.style.objectFit = "contain";
             weatherIcon.style.display = "block";
-            weatherIcon.style.margin = "37%"
+            weatherIcon.style.marginLeft = "37%"
             weatherIcon.src = getWeatherIcon(dateData.value);
             cell.appendChild(weatherIcon);
             rowElement.appendChild(cell);
