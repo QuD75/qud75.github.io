f79aab535684a5ca15ea35658229e6240f963598
Quentin Dusserre <quentin.dusserre@ekwateur.fr>
2024-11-03 17:42:01 +0100

    delete default color

--- js/color.js
+++ js/color.js
@@ -82,9 +82,3 @@ function getTextColor(color) {
     const luminosity = 0.299 * r + 0.587 * g + 0.114 * b;
     return luminosity < 105.4 ? 'white' : 'black';
 }
-function defaultColorFunc(value) {
-    return {
-        color: 'white',
-        textColor: 'black'
-    };
-}
