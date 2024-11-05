4096dfdee4c59f8792f201cdc652091aa0236cd2
Quentin Dusserre <quentin.dusserre@ekwateur.fr>
2024-11-05 21:37:21 +0100

    graphiques 24h

--- js/api_meteo.js
+++ js/api_meteo.js
@@ -163,10 +163,7 @@ document.addEventListener('DOMContentLoaded', () => {
         fillWindDirSymbol('wind-direction-24h-row', windDirectionToDisplay, '65%', 0);
         fillWeatherSymbol('weather-24h-row', weatherToDisplay, '100%', 0);
 
-        timesToDisplay.forEach(date => {
-            date = formatDate(date, false, false, false, true, false);
-        });
-
+        timesToDisplay = timesToDisplay.map(date => formatDate(date, false, false, false, true, false));
         createChartOM('temperature-day-chart', timesToDisplay, tempsToDisplay, 'Evolution de la température dans les prochaines 24h', 'Heure', 'Température (°C)', 0, 'line', 'rgba(255, 99, 132, 1)', 'rgba(255, 99, 132, 0.2)');
         createChartOM('precipitation-day-chart', timesToDisplay, rainToDisplay, 'Evolution des précipitations dans les prochaines 24h', 'Heure', 'Pluie (mm)', 1, 'bar', 'rgba(0, 0, 139, 1)', 'rgba(0, 0, 139, 0.2)');
         createChartOM('wind-day-chart', timesToDisplay, windSpeedToDisplay, 'Evolution du vent dans les prochaines 24h', 'Heure', 'Vent (km/h)', 0, 'line', 'rgba(204, 204, 0, 1)', 'rgba(255, 255, 0, 0.2)', windGustToDisplay);
