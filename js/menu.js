836a0991fe4a04cb770bd3457399ed36cdb52942
Quentin Dusserre <quentin.dusserre@ekwateur.fr>
2025-06-02 16:13:57 +0200

    css header menu footer

--- js/menu.js
+++ js/menu.js
@@ -1,24 +1,11 @@
-// Fonction pour charger le menu avec sessionStorage
-function loadMenu() {
-    // Vérifier si le menu est déjà dans le stockage de session
-    const storedMenu = sessionStorage.getItem('menuContent');
-
-    if (storedMenu) {
-        // Si le menu est trouvé dans le stockage de session, l'afficher
-        document.getElementById('menu-container').innerHTML = storedMenu;
-    } else {
-        // Si le menu n'est pas trouvé, le charger depuis le fichier
-        fetch('/pages/menu.html')
-            .then(response => response.text())
-            .then(data => {
-                // Stocker le contenu du menu dans le stockage de session
-                sessionStorage.setItem('menuContent', data);
-                // Afficher le menu
-                document.getElementById('menu-container').innerHTML = data;
-            })
-            .catch(error => console.error('Erreur lors du chargement du menu:', error));
-    }
-}
-
-// Appeler la fonction pour charger le menu
-loadMenu();
+document.getElementById('menu-container').innerHTML = `
+    <nav class="site-nav">
+        <ul class="nav-links">
+            <li><a href="/pages/previsions-meteo.html">Prévisions météo</a></li>
+            <li><a href="/pages/marees.html">Infos marées</a></li>
+            <li><a href="/pages/station-meteo.html">Station météo</a></li>
+            <li><a href="/pages/webcam.html">Webcam</a></li>
+            <li><a href="/pages/climat-croisic.html">Climat Croisic</a></li>
+        </ul>
+    </nav>
+`;
