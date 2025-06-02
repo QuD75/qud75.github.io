58a8d242668ad008141ce6b8848be1a9b6021ed7
Quentin Dusserre <quentin.dusserre@ekwateur.fr>
2025-06-02 10:42:17 +0200

    test vigilance

--- js/api_meteo.js
+++ js/api_meteo.js
@@ -1,7 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
     const lat = '47.29';
     const long = '-2.52';
-    const dep = '44';
+    //const dep = '44';
+    const dep = '66';
     const urlVigilance = `https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets/weatherref-france-vigilance-meteo-departement/records?where=domain_id=${dep}&limit=20`
     const urlOpenMeteoDay = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&hourly=temperature_2m,precipitation,weather_code,pressure_msl,wind_speed_10m,wind_direction_10m,wind_gusts_10m,uv_index,is_day&forecast_days=2&timezone=Europe%2FBerlin`;
     const urlOpenMeteoWeek = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&daily=weather_code,temperature_2m_max,temperature_2m_min,sunrise,sunset,uv_index_max,precipitation_sum,wind_gusts_10m_max&timezone=Europe%2FBerlin`;
