03701b515fc39669dd8cb0ea11cebe8e14942591
Quentin Dusserre <quentin.dusserre@ekwateur.fr>
2025-06-02 16:32:24 +0200

    format footer

--- js/footer.js
+++ js/footer.js
@@ -1,6 +1,6 @@
 document.getElementById('footer-container').innerHTML = `
     <footer class="site-footer">
-        <p>&copy; ${new Date().getFullYear()} Météo Croisic · Données fournies par Météo France</p>
+        <p>&copy; ${new Date().getFullYear()} Météo Croisic</p>
         <p><a href="/pages/mentions-legales.html">Mentions légales</a></p>
     </footer>
 `;
