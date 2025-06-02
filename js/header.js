0dac4c20a94b28f6200494b34674cf4032abac3d
Quentin Dusserre <quentin.dusserre@ekwateur.fr>
2025-06-02 16:18:43 +0200

    header centré

--- js/header.js
+++ js/header.js
@@ -2,7 +2,7 @@ document.getElementById('header-container').innerHTML = `
     <header class="site-header">
         <div class="logo-title">
             <img src="/icons/logos/Le_Croisic_logo.png" alt="Logo gauche" class="logo">
-            <a href="/index.html" style="text-decoration: none; color: inherit;">
+            <a href="/index.html" class="header-link">
                 <h1>Météo au Croisic</h1>
             </a>
             <img src="/icons/logos/Blason_Le_Croisic.png" alt="Logo droite" class="logo">
