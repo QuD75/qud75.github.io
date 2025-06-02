cc98d3f0c1a7cd524132578d5bdac999304b5bd4
Quentin Dusserre <quentin.dusserre@ekwateur.fr>
2025-06-02 16:16:50 +0200

    header cliquable

--- js/header.js
+++ js/header.js
@@ -2,7 +2,9 @@ document.getElementById('header-container').innerHTML = `
     <header class="site-header">
         <div class="logo-title">
             <img src="/icons/logos/Le_Croisic_logo.png" alt="Logo gauche" class="logo">
-            <h1>Météo au Croisic</h1>
+            <a href="/index.html" style="text-decoration: none; color: inherit;">
+                <h1>Météo au Croisic</h1>
+            </a>
             <img src="/icons/logos/Blason_Le_Croisic.png" alt="Logo droite" class="logo">
         </div>
     </header>
