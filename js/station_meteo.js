c3bb40be67256adeb2e8f99c94c641b30da05d89
Quentin Dusserre <quentin.dusserre@ekwateur.fr>
2025-06-03 10:20:23 +0200

    réorga des scripts

--- js/station_meteo.js
+++ js/station_meteo.js
@@ -1,8 +1,5 @@
-const url = "https://api.weather.com/v2/pws/observations/current?stationId=ILECRO29&format=json&units=m&apiKey=556a0b6740e249fdaa0b6740e2c9fdea&numericPrecision=decimal";
 
-fetch('https://api.weather.com/v2/pws/observations/current?stationId=ILECRO29&format=json&units=m&apiKey=556a0b6740e249fdaa0b6740e2c9fdea&numericPrecision=decimal')
-  .then(res => res.json())
-  .then(data => {
+function weatherStation(data){
     const obs = data.observations[0];
 
     const temperature = obs.metric.temp;
@@ -26,4 +23,4 @@ fetch('https://api.weather.com/v2/pws/observations/current?stationId=ILECRO29&fo
         ${obs.uv} UV
     `;
     document.getElementById("pluie-valeur").textContent = `${pluie} mm`;
-  });
+  }
