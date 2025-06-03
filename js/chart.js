6298392b13bc46e4c88e93826c06d5af18a50d79
Quentin Dusserre <quentin.dusserre@ekwateur.fr>
2025-06-03 11:44:46 +0200

    graph humidité

--- js/chart.js
+++ js/chart.js
@@ -1,53 +1,107 @@
 function fetchTemperatureDataAndCreateChart(data) {
-try {
-    const temperatureData = data.observations.map(entry => ({
-        time: entry.obsTimeLocal,
-        temp: entry.metric.tempAvg
-    }));
+    try {
+        const temperatureData = data.observations.map(entry => ({
+            time: entry.obsTimeLocal,
+            temp: entry.metric.tempAvg
+        }));
 
-    const labels = temperatureData.map(entry => {
-        const date = new Date(entry.time);
-        return date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-    });
+        const labels = temperatureData.map(entry => {
+            const date = new Date(entry.time);
+            return date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+        });
 
-    const tempValues = temperatureData.map(entry => entry.temp);
-  
-      const chartData = {
-        labels: labels,
-        datasets: [{
-          label: 'Température moyenne (°C)',
-          data: tempValues,
-          borderColor: 'rgba(255, 99, 132, 1)',
-          backgroundColor: 'rgba(255, 99, 132, 0.2)',
-          fill: true,
-          tension: 0.3
-        }]
-      };
-  
-      const config = {
-        type: 'line',
-        data: chartData,
-        options: {
-          responsive: true,
-          scales: {
-            y: {
-              title: {
-                display: true,
-                text: 'Température (°C)'
-              }
-            },
-            x: {
-              title: {
-                display: true,
-                text: 'Heure'
-              }
+        const tempValues = temperatureData.map(entry => entry.temp);
+    
+        const chartData = {
+            labels: labels,
+            datasets: [{
+            label: 'Température (°C)',
+            data: tempValues,
+            borderColor: 'rgba(255, 99, 132, 1)',
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            fill: true,
+            tension: 0.3
+            }]
+        };
+    
+        const config = {
+            type: 'line',
+            data: chartData,
+            options: {
+            responsive: true,
+            scales: {
+                y: {
+                title: {
+                    display: true,
+                    text: 'Température (°C)'
+                }
+                },
+                x: {
+                title: {
+                    display: true,
+                    text: 'Heure'
+                }
+                }
             }
-          }
-        }
-      };
-  
-      new Chart(document.getElementById('temperatureChart'), config);
+            }
+        };
+    
+        new Chart(document.getElementById('temperatureChart'), config);
     } catch (error) {
-      console.error('Erreur lors de la récupération des données température :', error);
+    console.error('Erreur lors de la récupération des données température :', error);
     }
-  }
+}
+
+function fetchHumidityDataAndCreateChart(data) {
+    try {
+        const humidityData = data.observations.map(entry => ({
+            time: entry.obsTimeLocal,
+            hum: entry.humidityAvg
+        }));
+
+        const labels = humidityData.map(entry => {
+            const date = new Date(entry.time);
+            return date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+        });
+
+        const humidityValues = humidityData.map(entry => entry.hum);
+    
+        const chartData = {
+            labels: labels,
+            datasets: [{
+            label: 'Humidité (%)',
+            data: humidityValues,
+            borderColor: 'rgb(99, 159, 255)',
+            backgroundColor: 'rgba(60, 87, 218, 0.2)',
+            fill: true,
+            tension: 0.3
+            }]
+        };
+    
+        const config = {
+            type: 'line',
+            data: chartData,
+            options: {
+            responsive: true,
+            scales: {
+                y: {
+                title: {
+                    display: true,
+                    text: 'Humidité (%)'
+                }
+                },
+                x: {
+                title: {
+                    display: true,
+                    text: 'Heure'
+                }
+                }
+            }
+            }
+        };
+    
+        new Chart(document.getElementById('humidityChart'), config);
+    } catch (error) {
+    console.error('Erreur lors de la récupération des données humidité :', error);
+    }
+}
