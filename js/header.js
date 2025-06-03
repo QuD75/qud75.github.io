a666832702c6f2adae4e00848e48eea1becaba26
Quentin Dusserre <quentin.dusserre@ekwateur.fr>
2025-06-03 14:33:38 +0200

    logo en webp

---
+++ js/header.js
@@ -0,0 +1,19 @@
+const headerKey = "cachedHeader";
+
+if (sessionStorage.getItem(headerKey)) {
+  document.getElementById("header-container").innerHTML = sessionStorage.getItem(headerKey);
+} else {
+  const html = `
+    <header class="site-header">
+        <div class="logo-title">
+            <img src="/icons/logos/Le_Croisic_logo.png" alt="Logo gauche" class="logo">
+            <a href="/index.html" class="header-link">
+                <h1>Météo au Croisic</h1>
+            </a>
+            <img src="/icons/logos/Blason_Le_Croisic.png" alt="Logo droite" class="logo">
+        </div>
+    </header>
+  `;
+  document.getElementById("header-container").innerHTML = html;
+  sessionStorage.setItem(headerKey, html);
+}
