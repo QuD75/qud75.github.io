4e6d5a44a7ce6f3f63594e98c2b4f8f7d0d6c371
Quentin Dusserre <quentin.dusserre@ekwateur.fr>
2025-06-03 10:30:15 +0200

    mise en cache header menu et footer

--- js/header.js
+++ js/header.js
@@ -1,11 +1,20 @@
-document.getElementById('header-container').innerHTML = `
-    <header class="site-header">
-        <div class="logo-title">
-            <img src="/icons/logos/Le_Croisic_logo.png" alt="Logo gauche" class="logo">
-            <a href="/index.html" class="header-link">
-                <h1>Météo au Croisic</h1>
-            </a>
-            <img src="/icons/logos/Blason_Le_Croisic.png" alt="Logo droite" class="logo">
-        </div>
-    </header>
-`;
+const headerKey = "cachedHeader";
+const cached = localStorage.getItem(headerKey);
+
+if (cached) {
+    document.getElementById("header-container").innerHTML = cached;
+} else {
+    const html = `
+        <header class="site-header">
+            <div class="logo-title">
+                <img src="/icons/logos/Le_Croisic_logo.png" alt="Logo gauche" class="logo">
+                <a href="/index.html" class="header-link">
+                    <h1>Météo au Croisic</h1>
+                </a>
+                <img src="/icons/logos/Blason_Le_Croisic.png" alt="Logo droite" class="logo">
+            </div>
+        </header>
+    `;
+    document.getElementById("header-container").innerHTML = html;
+    localStorage.setItem(headerKey, html);
+}
