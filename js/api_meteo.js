7fe0007e2a34e83288d2accd8381258cdc720320
Quentin Dusserre <quentin.dusserre@ekwateur.fr>
2025-06-03 10:20:23 +0200

    réorga des scripts

--- js/api_meteo.js
+++ js/api_meteo.js
@@ -6,11 +6,13 @@ document.addEventListener('DOMContentLoaded', () => {
     const urlVigilance = `https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets/weatherref-france-vigilance-meteo-departement/records?where=domain_id=${dep}&limit=20`
     const urlOpenMeteoDay = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&hourly=temperature_2m,precipitation,weather_code,pressure_msl,wind_speed_10m,wind_direction_10m,wind_gusts_10m,uv_index,is_day&forecast_days=2&timezone=Europe%2FBerlin`;
     const urlOpenMeteoWeek = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&daily=weather_code,temperature_2m_max,temperature_2m_min,sunrise,sunset,uv_index_max,precipitation_sum,wind_gusts_10m_max&timezone=Europe%2FBerlin`;
+    const api_station_meteo = "https://api.weather.com/v2/pws/observations/current?stationId=ILECRO29&format=json&units=m&apiKey=556a0b6740e249fdaa0b6740e2c9fdea&numericPrecision=decimal";
 
     // Appel aux API
     async function getApiData() {
         // Appels API indépendants
         fetchData(urlVigilance, 'vigilance', 0, displayDataVigilance);
+        fetchData(api_station_meteo, 'station_meteo', 1/60, weatherStation);
     }
     async function fetchData(apiUrl, cacheKey, duration, displayFunction) {
         const now = Date.now();
