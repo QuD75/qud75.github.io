57f6dcdfe14915a41ac1402a02a1a9de2d91a253
Quentin Dusserre <quentin.dusserre@ekwateur.fr>
2025-06-03 15:51:10 +0200

    barre UV

--- js/station_meteo.js
+++ js/station_meteo.js
@@ -8,6 +8,11 @@ function weatherStation(data){
     const vent = obs.metric.windSpeed;
     const lumiere = obs.solarRadiation;
     const pluie = obs.metric.precipTotal;
+    const windDir = obs.winddir;
+    const windDGust = obs.metric.windGust;
+    const uv = obs.uv;
+    const uvMax = 11;
+    const uvPercentage = Math.min((uv / uvMax) * 100, 100); // max 100%
 
     document.getElementById("temperature-valeur").textContent = `${temperature} °C`;
     document.getElementById("temperature-valeur").style.color = getTemperatureColor(temperature);
@@ -15,12 +20,12 @@ function weatherStation(data){
     document.getElementById("humidite-valeur").textContent = `${humidite} %`;
     document.getElementById("vent-valeur").innerHTML = `
         ${vent} km/h<br>
-        ${obs.metric.windGust} km/h (rafales)
+        ${windDGust} km/h (rafales)
     `;
-    document.getElementById("wind-arrow").style.transform = `rotate(${obs.winddir}deg)`;
-    document.getElementById("lumiere-valeur").innerHTML = `
+    document.getElementById("wind-arrow").style.transform = `rotate(${windDir}deg)`;
+    document.getElementById("lumiere-valeur-nombre").innerHTML = `
         ${Math.round(lumiere)} W/m²<br>
-        ${obs.uv} UV
     `;
+    document.getElementById("uv-bar").style.width = uvPercentage+"%";
     document.getElementById("pluie-valeur").textContent = `${pluie} mm`;
   }
