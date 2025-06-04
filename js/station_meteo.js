ce3e448cdd7018f543205056112a2e9ed6888f49
Quentin Dusserre <quentin.dusserre@ekwateur.fr>
2025-06-04 11:10:17 +0200

    unités plus petit

--- js/station_meteo.js
+++ js/station_meteo.js
@@ -28,7 +28,7 @@ function weatherStation(data){
     const uvBar = document.getElementById("uv-bar");
     uvBar.style.width = `${uvPercentage}%`;
     uvBar.style.background = `linear-gradient(to right, ${uvColors.slice(0, Math.ceil(uv / 2 + 1)).join(", ")})`;
-    document.getElementById("pluie-valeur").textContent = `${pluie.toFixed(1)}&nbsp;<span class="unit-small">mm</span>`;
+    document.getElementById("pluie-valeur").innerHTML = `${pluie.toFixed(1)}&nbsp;<span class="unit-small">mm</span>`;
   }
 
   const uvColors = [
