f4399f662204d948e17f8611c4e63a1f01a4097a
Quentin Dusserre <quentin.dusserre@ekwateur.fr>
2025-06-11 17:12:59 +0200

    tides

--- js/api_meteo.js
+++ js/api_meteo.js
@@ -1,18 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    const today = new Date();
-    const tomorrow = new Date();
-    tomorrow.setDate(today.getDate() + 3);
-    // Fonction pour formater une date en YYYY-MM-DD
-    function formatDate(date) {
-        return date.toISOString().split('T')[0]; // extrait juste la partie date
-    }
-    const startDate = formatDate(today);
-    const endDate = formatDate(tomorrow);
-
     const stationId = 'ILECRO29';
     const stationApiKey = '556a0b6740e249fdaa0b6740e2c9fdea';
-    const tidesApiKey = '8b2d7d44-469d-11f0-976d-0242ac130006-8b2d7db2-469d-11f0-976d-0242ac130006';
     
     const lat = '47.29';
     const long = '-2.52';
@@ -24,7 +13,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const api_station_meteo = `https://api.weather.com/v2/pws/observations/current?stationId=${stationId}&format=json&units=m&apiKey=${stationApiKey}&numericPrecision=decimal`;
     const api_station_meteo_day = `https://api.weather.com/v2/pws/observations/all/1day?stationId=${stationId}&format=json&units=m&apiKey=${stationApiKey}&numericPrecision=decimal`;
     const api_station_meteo_week = `https://api.weather.com/v2/pws/observations/hourly/7day?stationId=${stationId}&format=json&units=m&apiKey=${stationApiKey}&numericPrecision=decimal`;
-    //const api_tides = `https://api.stormglass.io/v2/tide/extremes/point?lat=${lat}&lng=${long}&start=${startDate}&end=${endDate}`;
 
     // Appel aux API
     async function getApiData() {
@@ -33,10 +21,9 @@ document.addEventListener('DOMContentLoaded', () => {
         fetchData(api_station_meteo, 'station_meteo', 1, weatherStation, false);
         fetchData(api_station_meteo_day, 'station_meteo_day', 1, fetchDataWeatherStationAndCreateChartsDay, false);
         fetchData(api_station_meteo_week, 'station_meteo_week', 1, fetchDataWeatherStationAndCreateChartsWeek, false);
-        fetchData(api_tides, 'tides', 120, tides, tidesApiKey, true);
     }
 
-    async function fetchData(apiUrl, cacheKey, duration, displayFunction, header, isMock) {
+    async function fetchData(apiUrl, cacheKey, duration, displayFunction) {
         const now = Date.now();
         const cachedData = JSON.parse(localStorage.getItem(cacheKey));
         if (cachedData && (now - cachedData.timestamp < duration * 60000)) {
@@ -44,25 +31,7 @@ document.addEventListener('DOMContentLoaded', () => {
             return cachedData.data;
         }
         try {
-            // Prépare l'objet des options de fetch
-            const options = {
-                method: "GET",
-                headers: {}
-            };
-            // Si un header est fourni, on l’ajoute
-            if (header) {
-                options.headers["Authorization"] = header;
-            }
-            // Lance le fetch avec ou sans headers
-            let response;
-            if (isMock) {
-                response = {
-                    ok: true,
-                    json: async () => ({}) // renvoie un objet vide simulé
-                };
-            } else {
-                response = await fetch(apiUrl, options);
-            } 
+            const response = await fetch(apiUrl); 
             if (!response.ok) throw new Error(`HTTP Error: ${response.status}`);
             const data = await response.json();
             localStorage.setItem(cacheKey, JSON.stringify({ data: data, timestamp: now }));
