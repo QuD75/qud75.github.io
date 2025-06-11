8401919411c1f3a1d5e2ee0924ef540f93630db4
Quentin Dusserre <quentin.dusserre@ekwateur.fr>
2025-06-11 14:54:39 +0200

    tides

--- js/tides.js
+++ js/tides.js
@@ -1,4 +1,4 @@
-const dataMock = `{
+const dataMock = {
    "data":[
       {
          "height":1.665780660749532,
@@ -80,33 +80,22 @@ const dataMock = `{
       }
    }
 }
-`
 
 function tides(data){
-    const parsed = JSON.parse(dataMock);
-    const tide1 = parsed.data[0].time;
-    const tide2 = parsed.data[1].time;
-    const tide3 = parsed.data[2].time;
-    const tide4 = parsed.data[3].time;
-    const tide5 = parsed.data[4].time;
-    const tide6 = parsed.data[5].time;
-    const tide7 = parsed.data[6].time;
-    const tide8 = parsed.data[7].time;
-    const tide9 = parsed.data[8].time;
-    const tide10 = parsed.data[9].time;
-    const tide11 = parsed.data[10].time;
-    const tide12 = parsed.data[11].time;
-
-    document.getElementById("tide1").textContent = `${new Date(tide1).toLocaleString()}`;
-    document.getElementById("tide2").textContent = `${new Date(tide2).toLocaleString()}`;
-    document.getElementById("tide3").textContent = `${new Date(tide3).toLocaleString()}`;
-    document.getElementById("tide4").textContent = `${new Date(tide4).toLocaleString()}`;
-    document.getElementById("tide5").textContent = `${new Date(tide5).toLocaleString()}`;
-    document.getElementById("tide6").textContent = `${new Date(tide6).toLocaleString()}`;
-    document.getElementById("tide7").textContent = `${new Date(tide7).toLocaleString()}`;
-    document.getElementById("tide8").textContent = `${new Date(tide8).toLocaleString()}`;
-    document.getElementById("tide9").textContent = `${new Date(tide9).toLocaleString()}`;
-    document.getElementById("tide10").textContent = `${new Date(tide10).toLocaleString()}`;
-    document.getElementById("tide11").textContent = `${new Date(tide11).toLocaleString()}`;
-    document.getElementById("tide12").textContent = `${new Date(tide12).toLocaleString()}`;
+    const tableBody = document.getElementById("maree-table-body");
+    dataMock.data.forEach(entry => {
+        const row = document.createElement("tr");
+  
+        const time = new Date(entry.time);
+        const localTime = time.toLocaleString("fr-FR", { timeZone: "Europe/Paris" });
+  
+        const type = entry.type === "high" ? "Marée haute" : "Marée basse";
+  
+        row.innerHTML = `
+          <td>${localTime}</td>
+          <td>${type}</td>
+          <td>${entry.height.toFixed(2)}</td>
+        `;
+        tableBody.appendChild(row);
+      });
 }
