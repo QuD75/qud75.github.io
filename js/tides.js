1ae558f35f1369b688e1dc5ee287815d7bd53d48
Quentin Dusserre <quentin.dusserre@ekwateur.fr>
2025-06-12 15:28:03 +0200

    tides

--- js/tides.js
+++ js/tides.js
@@ -1,30 +1,32 @@
-function updateTimeline() {
-    console.log("updateTimeline called");
-    const maregrammeRoot = document.getElementById("maregramme-root");
+window.addEventListener('DOMContentLoaded', () => {
+    function updateTimeline() {
+        console.log("updateTimeline called");
+        const maregrammeRoot = document.getElementById("maregramme-root");
 
-    if (maregrammeRoot) {
-        console.log("maregrammeRoot", maregrammeRoot.innerHTML);
-    } else {
-        console.warn("Element with id 'maregramme-root' not found");
-    }
-}
-
-const tideContainer = document.getElementById("tide-container");
-
-if (tideContainer) {
-    const observer = new MutationObserver((mutationsList, observerInstance) => {
-        const target = document.getElementById("maregramme-root");
-        if (target) {
-            console.log("Élément détecté !");
-            updateTimeline();
-            observerInstance.disconnect();
+        if (maregrammeRoot) {
+            console.log("maregrammeRoot", maregrammeRoot.innerHTML);
+        } else {
+            console.warn("Element with id 'maregramme-root' not found");
         }
-    });
+    }
 
-    observer.observe(tideContainer, {
-        childList: true,
-        subtree: true
-    });
-} else {
-    console.warn("Le conteneur #tide-container est introuvable");
-}
+    const tideContainer = document.getElementById("tide-container");
+
+    if (tideContainer) {
+        const observer = new MutationObserver((mutationsList, observerInstance) => {
+            const target = document.getElementById("maregramme-root");
+            if (target) {
+                console.log("Élément détecté !");
+                updateTimeline();
+                observerInstance.disconnect();
+            }
+        });
+
+        observer.observe(tideContainer, {
+            childList: true,
+            subtree: true
+        });
+    } else {
+        console.warn("Le conteneur #tide-container est introuvable");
+    }
+});
