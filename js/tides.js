a5d099229b4fd861215486db1ad35157a38f53ef
Quentin Dusserre <quentin.dusserre@ekwateur.fr>
2025-06-12 15:50:00 +0200

    tides

--- js/tides.js
+++ js/tides.js
@@ -1,14 +1,7 @@
-function tryUpdateTimeline() {
-    const maregrammeRoot = document.getElementById("maregramme-root");
-    if (maregrammeRoot) {
-      console.log("maregrammeRoot trouvé !");
-      updateTimeline();
-    } else {
-      console.log("maregrammeRoot pas encore là, je réessaie dans 1s...");
-      setTimeout(tryUpdateTimeline, 1000);
-    }
-  }
+function updateTimeline() {
+
+}
   
-  window.addEventListener('DOMContentLoaded', () => {
-    tryUpdateTimeline();
-  });
+window.addEventListener('DOMContentLoaded', () => {
+updateTimeline();
+});
