b1939ab7b188dda61936b1714d5f392f890d1feb
Quentin Dusserre <quentin.dusserre@ekwateur.fr>
2025-06-17 17:29:22 +0200

    weather forecast

--- js/weather-forecast.js
+++ js/weather-forecast.js
@@ -1,29 +1,32 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     const apikeyOpenMeteoMap = '3019a6c49cea102650053a8919b5fa54';
+    const apiKeyMeteoConcept = 'e0e611ffab773b1d4a1d6355862317ec39ea907e1f590a10a01ae07cc49ea9d8';
     const baseUrlOpenMeteoMap = 'https://api.openweathermap.org/data/3.0/onecall';
+    const baseUrLMeteoConcept = 'http://api.meteo-concept.com/api/forecast/nextHours';
     const lat = 47.29;
     const lon = -2.52;
+    const insee = '44049';
     const exclude = 'current,minutely,alerts';
     const units = 'metric';
 
     const openMeteoMapApi = `${baseUrlOpenMeteoMap}?lat=${lat}&lon=${lon}&exclude=${exclude}&units=${units}&appid=${apikeyOpenMeteoMap}`;
-    //const meteoConceptApi = `${baseUrlOpenMeteoMap}?lat=${lat}&lon=${lon}&exclude=${exclude}&units=${units}&appid=${apikeyOpenMeteoMap}`;
+    const meteoConceptApi = `${baseUrLMeteoConcept}?token=${apiKeyMeteoConcept}&insee=${insee}&hourly=true`;
   
     async function getApiData() {
         fetchData(openMeteoMapApi, 'open_meteo_map', 0, getOpenMeteoMapData); 
-        //fetchData(meteoConceptApi, 'meteo_concept', 0, getMeteoConceptData);
+        fetchData(meteoConceptApi, 'meteo_concept', 0, getMeteoConceptData);
     }
   
     getApiData();
     
-});
+});  
 
-/* function getWeatherForecastData(data){
-    const tbody = document.querySelector('tbody');
+function getOpenMeteoMapData(data){
+  const tbody = document.querySelector('tbody');
 
-    // Regrouper les données par jour
-    const grouped = {};
+  // Regrouper les données par jour
+  const grouped = {};
 
   data.hourly.forEach(h => {
     const date = new Date(h.dt * 1000);
@@ -35,12 +38,63 @@ document.addEventListener('DOMContentLoaded', () => {
     if (!grouped[jour]) grouped[jour] = [];
     grouped[jour].push({
       heure,
-      tempC: Math.round(h.temp),
-      humidity: h.humidity,
       pressure: h.pressure,
-      wind_speed: Math.round(h.wind_speed*3.6),
       uvi: Math.round(h.uvi),
-      description: h.weather[0].description
+    });
+  });
+
+  // Générer les lignes du tableau avec rowspan
+  for (const [jour, heures] of Object.entries(grouped)) {
+    heures.forEach((entry, index) => {
+      const row = document.createElement("tr");
+
+      if (index === 0) {
+        const jourCell = document.createElement("td");
+        jourCell.rowSpan = heures.length;
+        jourCell.textContent = jour;
+        row.appendChild(jourCell);
+      }
+
+      row.innerHTML += `
+        <td>${entry.heure}</td>
+        <td></td>
+        <td></td>
+        <td>${entry.pressure}</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td>${entry.uvi}</td>
+        <td></td>
+      `;
+
+      tbody.appendChild(row);
+    });
+  }
+}
+
+function getMeteoConceptData(data){
+  const tbody = document.querySelector('tbody');
+
+  // Regrouper les données par jour
+  const grouped = {};
+
+  data.forecast.forEach(h => {
+    const date = new Date(h.datetime);
+    const optionsJour = { day: '2-digit', month: '2-digit', year: 'numeric'};
+    const optionsHeure = { hour: '2-digit', minute: '2-digit'};
+    const jour = date.toLocaleDateString('fr-FR', optionsJour);
+    const heure = date.toLocaleTimeString('fr-FR', optionsHeure);
+
+    if (!grouped[jour]) grouped[jour] = [];
+    grouped[jour].push({
+      heure,
+      tempC: Math.round(h.temp2m),
+      humidity: h.rh2m,
+      wind_speed: h.wind10m,
+      wind_gust: h.gust10m,
+      wind_direction: h.dirwind10m,
+      rain: h.rr10,
+      description: h.weather
     });
   });
 
@@ -60,63 +114,15 @@ document.addEventListener('DOMContentLoaded', () => {
         <td>${entry.heure}</td>
         <td>${entry.tempC}</td>
         <td>${entry.humidity}</td>
-        <td>${entry.pressure}</td>
+        <td></td>
         <td>${entry.wind_speed}</td>
-        <td>${entry.uvi}</td>
+        <td>${entry.wind_gust}</td>
+        <td>${entry.wind_direction}</td>
+        <td></td>
         <td>${entry.description}</td>
       `;
 
       tbody.appendChild(row);
     });
   }
-
-} */
-
-function getOpenMeteoMapData(data){
-  const tbody = document.querySelector('tbody');
-
-  // Regrouper les données par jour
-  const grouped = {};
-
-data.hourly.forEach(h => {
-  const date = new Date(h.dt * 1000);
-  const optionsJour = { day: '2-digit', month: '2-digit', year: 'numeric', timeZone: data.timezone };
-  const optionsHeure = { hour: '2-digit', minute: '2-digit', timeZone: data.timezone };
-  const jour = date.toLocaleDateString('fr-FR', optionsJour);
-  const heure = date.toLocaleTimeString('fr-FR', optionsHeure);
-
-  if (!grouped[jour]) grouped[jour] = [];
-  grouped[jour].push({
-    heure,
-    pressure: h.pressure,
-    uvi: Math.round(h.uvi),
-  });
-});
-
-// Générer les lignes du tableau avec rowspan
-for (const [jour, heures] of Object.entries(grouped)) {
-  heures.forEach((entry, index) => {
-    const row = document.createElement("tr");
-
-    if (index === 0) {
-      const jourCell = document.createElement("td");
-      jourCell.rowSpan = heures.length;
-      jourCell.textContent = jour;
-      row.appendChild(jourCell);
-    }
-
-    row.innerHTML += `
-      <td>${entry.heure}</td>
-      <td></td>
-      <td></td>
-      <td>${entry.pressure}</td>
-      <td></td>
-      <td>${entry.uvi}</td>
-      <td></td>
-    `;
-
-    tbody.appendChild(row);
-  });
-}
-
 }
