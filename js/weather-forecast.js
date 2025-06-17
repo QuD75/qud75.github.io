0c48e9adab41638592a15413ae8e2ae086311d29
Quentin Dusserre <quentin.dusserre@ekwateur.fr>
2025-06-17 16:27:50 +0200

    weather forecast

--- js/weather-forecast.js
+++ js/weather-forecast.js
@@ -17,5 +17,30 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 function getWeatherForecastData(data){
+    const tbody = document.querySelector('tbody');
+
+    data.hourly.forEach(h => {
+        const d = new Date(h.dt * 1000);
+        const hour = d.toLocaleString('fr-FR', {
+          year: 'numeric', month: '2-digit', day: '2-digit',
+          hour: '2-digit', minute: '2-digit',
+          timeZone: data.timezone
+        });
+    
+        // conversion Kelvin -> °C
+        const tempC = (h.temp - 273.15).toFixed(1);
+    
+        const row = document.createElement('tr');
+        row.innerHTML = `
+          <td>${hour}</td>
+          <td>${tempC}</td>
+          <td>${h.humidity}</td>
+          <td>${h.pressure}</td>
+          <td>${h.wind_speed.toFixed(1)}</td>
+          <td>${h.uvi.toFixed(1)}</td>
+          <td>${h.weather[0].description}</td>
+        `;
+        tbody.appendChild(row);
+      });
 
 }
