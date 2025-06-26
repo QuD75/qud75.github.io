b0ecc26df83d630d5be3d7bc7676e49be2376eae
Quentin Dusserre <quentin.dusserre@ekwateur.fr>
2025-06-26 09:53:13 +0200

    css forecast

--- js/weather-forecast.js
+++ js/weather-forecast.js
@@ -32,7 +32,7 @@ document.addEventListener('DOMContentLoaded', () => {
       document.querySelectorAll('h2').forEach(h2 => {
         h2.style.display = 'block';
       });
-      document.getElementById('forecast-day').style.display = 'flex';
+      document.getElementById('forecast-day').style.display = 'block';
       document.getElementById('forecast-week').style.display = 'block';
     }
   
