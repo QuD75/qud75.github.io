66031670ae7d302c2cfd3a3bc1b4848857c87fb2
Quentin Dusserre <quentin.dusserre@ekwateur.fr>
2025-07-02 16:42:02 +0200

    script esm.sh

--- js/weather-forecast.js
+++ js/weather-forecast.js
@@ -2,7 +2,6 @@ import { Chart, registerables } from "chart.js";
 Chart.register(...registerables);
 Chart.defaults.animation = false;
 
-//import { fr } from "date-fns/locale/fr";
 import "chartjs-adapter-date-fns";
 
 document.addEventListener('DOMContentLoaded', () => {
